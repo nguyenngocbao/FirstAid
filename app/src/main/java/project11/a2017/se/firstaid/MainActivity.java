1947b3ca6ed5922b12db113482923ee363587b4f
ngocbao <ngocbaostar787>
2017-04-21 16:33:03 +0700

    start

--- app/src/main/java/project11/a2017/se/firstaid/MainActivity.java
+++ app/src/main/java/project11/a2017/se/firstaid/MainActivity.java
@@ -24,47 +24,39 @@ public class MainActivity extends AppCompatActivity {
         mLam.dispatchCreate(savedInstanceState);
         tabHost =(TabHost) findViewById(R.id.tabHost);
 
-
         tabHost.setup(mLam);
-
 
         createAppBar();
         createTabHost();
-
     }
     public void createAppBar(){
         toolbar = (Toolbar)findViewById(R.id.toolBar);
       setSupportActionBar(toolbar);
-
-
-
     }
+
     public void createTabHost(){
-
-
         //tab 1
         TabHost.TabSpec tab1 = tabHost.newTabSpec("Sơ Cứu");
         tab1.setIndicator("Sơ cứu");
-       Intent tabIntent1 = new Intent(this,Tab1.class);
-
+        Intent tabIntent1 = new Intent(this,Tab1.class);
         tab1.setContent(tabIntent1);
         tabHost.addTab(tab1);
+
         //tab 2
         TabHost.TabSpec tab2 = tabHost.newTabSpec("Phòng bị");
         tab2.setIndicator("Phòng Bị");
         Intent tabIntent2 = new Intent(this,Tab2.class);
         tab2.setContent(tabIntent2);
-
-
         tabHost.addTab(tab2);
+
         //tab 3
         TabHost.TabSpec tab3 = tabHost.newTabSpec("Bệnh viện");
         tab3.setIndicator("Bệnh Viện");
         Intent tabIntent3 = new Intent(this,Hospital.class);
         tab3.setContent(tabIntent3);
         tabHost.addTab(tab3);
-
     }
+
     @Override
     protected void onResume(){
         super.onResume();
