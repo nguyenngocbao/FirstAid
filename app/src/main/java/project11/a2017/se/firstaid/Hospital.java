73345c1f5bf7d0cfaf3982a07c35d08b8fb4c3a1
ngocbao <ngocbaostar787>
2017-04-22 15:47:00 +0700

    start

--- app/src/main/java/project11/a2017/se/firstaid/Hospital.java
+++ app/src/main/java/project11/a2017/se/firstaid/Hospital.java
@@ -54,7 +54,9 @@ public class Hospital extends AppCompatActivity implements OnMapReadyCallback, V
                     ACCESS_FINE_LOCATION};
 
             // Hiển thị một Dialog hỏi người dùng cho phép các quyền trên.
-            ActivityCompat.requestPermissions(new MainActivity(),permissions,REQUEST_ID_ACCESS_COURSE_FINE_LOCATION);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(permissions,REQUEST_ID_ACCESS_COURSE_FINE_LOCATION);
+            }
 
             LatLng sydney = new LatLng(-34, 151);
             map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
@@ -84,120 +86,7 @@ public class Hospital extends AppCompatActivity implements OnMapReadyCallback, V
 
 
     }
-    private void askPermissionsAndShowMyLocation() {
 
-
-        // Với API >= 23, bạn phải hỏi người dùng cho phép xem vị trí của họ.
-        if (Build.VERSION.SDK_INT >= 23) {
-            int accessCoarsePermission
-                    = ContextCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION);
-            int accessFinePermission
-                    = ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION);
-
-
-            if (accessCoarsePermission != PERMISSION_GRANTED
-                    || accessFinePermission != PERMISSION_GRANTED) {
-
-                // Các quyền cần người dùng cho phép.
-                String[] permissions = new String[]{ACCESS_COARSE_LOCATION,
-                        ACCESS_FINE_LOCATION};
-
-                // Hiển thị một Dialog hỏi người dùng cho phép các quyền trên.
-                ActivityCompat.requestPermissions(this,permissions,REQUEST_ID_ACCESS_COURSE_FINE_LOCATION);
-
-                return;
-            }
-        }
-
-        // Hiển thị vị trí hiện thời trên bản đồ.
-        this.showMyLocation();
-    }
-    // Tìm một nhà cung cấp vị trị hiện thời đang được mở.
-    private String getEnabledLocationProvider() {
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-
-        // Tiêu chí để tìm một nhà cung cấp vị trí.
-        Criteria criteria = new Criteria();
-
-        // Tìm một nhà cung vị trí hiện thời tốt nhất theo tiêu chí trên.
-        // ==> "gps", "network",...
-        String bestProvider = locationManager.getBestProvider(criteria, true);
-
-        boolean enabled = locationManager.isProviderEnabled(bestProvider);
-
-        if (!enabled) {
-            Toast.makeText(this, "No location provider enabled!", Toast.LENGTH_LONG).show();
-            Log.i(MYTAG, "No location provider enabled!");
-            return null;
-        }
-        return bestProvider;
-    }
-
-    private void showMyLocation() {
-
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        String locationProvider = this.getEnabledLocationProvider();
-
-        if (locationProvider == null) {
-            return;
-        }
-
-        // Millisecond
-        final long MIN_TIME_BW_UPDATES = 1000;
-        // Met
-        final float MIN_DISTANCE_CHANGE_FOR_UPDATES = 1;
-
-        Location myLocation = null;
-        try {
-
-            // Đoạn code nay cần người dùng cho phép (Hỏi ở trên ***).
-            locationManager.requestLocationUpdates(
-                    locationProvider,
-                    MIN_TIME_BW_UPDATES,
-                    MIN_DISTANCE_CHANGE_FOR_UPDATES, (LocationListener) this);
-
-            // Lấy ra vị trí.
-            myLocation = locationManager
-                    .getLastKnownLocation(locationProvider);
-        }
-        // Với Android API >= 23 phải catch SecurityException.
-        catch (SecurityException e) {
-            Toast.makeText(this, "Show My Location Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
-            Log.e(MYTAG, "Show My Location Error:" + e.getMessage());
-            e.printStackTrace();
-            return;
-        }
-
-        if (myLocation != null) {
-
-            LatLng latLng = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
-            map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
-
-            CameraPosition cameraPosition = new CameraPosition.Builder()
-
-                    .target(latLng)             // Sets the center of the map to location user
-                    .zoom(15)                   // Sets the zoom
-                    .bearing(90)                // Sets the orientation of the camera to east
-                    .tilt(40)                   // Sets the tilt of the camera to 30 degrees
-                    .build();                   // Creates a CameraPosition from the builder
-            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-            // Thêm Marker cho Map:
-            MarkerOptions option = new MarkerOptions();
-            option.title("My Location");
-            option.snippet("....");
-            option.position(latLng);
-            Marker currentMarker = map.addMarker(option);
-            currentMarker.showInfoWindow();
-        } else {
-            Toast.makeText(this, "Location not found!", Toast.LENGTH_LONG).show();
-            Log.i(MYTAG, "Location not found");
-        }
-
-
-    }
 
 
 
