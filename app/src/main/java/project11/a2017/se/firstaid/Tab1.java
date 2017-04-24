64b22b1252883e126e6ddb3161716140cc0e57c3
ngocbao <ngocbaostar787>
2017-04-24 11:54:37 +0700

    start

--- app/src/main/java/project11/a2017/se/firstaid/Tab1.java
+++ app/src/main/java/project11/a2017/se/firstaid/Tab1.java
@@ -1,14 +1,26 @@
 package project11.a2017.se.firstaid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Tab1 extends Activity {
+    Button btn ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab1);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(this,Test.class);
+
+            }
+        });
     }
 }
